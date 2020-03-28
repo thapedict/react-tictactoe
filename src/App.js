c2bd7a8b900a49f9efab5af3808f2d00fc73ecd9
Thapelo Moeti <benzitto@gmail.com>
2020-03-28 13:58:13 +0200

    added class def

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import './App.css';
 
+class App extends React.Component {
+  constructor(props) {}
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
+}
+
+export default App;
